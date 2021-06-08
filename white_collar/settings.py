e953ba62626a4ea905bea154fea9587d7b5bf54e
prasan92 <you@example.com>
2021-06-08 16:28:08 +0530

    udated settings

--- white_collar/settings.py
+++ white_collar/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i&rjiyi$(4hu+=9k94p17=d!jeto%vn*ohayidy-=1r)7m(mya'
+#SECRET_KEY = 'i&rjiyi$(4hu+=9k94p17=d!jeto%vn*ohayidy-=1r)7m(mya'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anyonecanblog.herokuapp.com']
 
 
 # Application definition
